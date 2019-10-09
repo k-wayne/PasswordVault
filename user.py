7ea0ce135c3c717b6a79facf64210b6b3a66cd2d
kwayne <wkudoi@gmail.com>
2019-10-09 10:03:25 +0300

    created the user class

---
+++ user.py
@@ -0,0 +1,12 @@
+import string
+import random
+import secrets
+import pyperclip
+
+class User:
+    """
+    #this class creates an instance of a user
+    """
+    user_list = [] #set an empty array of expected users
+
+    def __init__(self, first_name, last_name, user_name, phone_number, email, password):
