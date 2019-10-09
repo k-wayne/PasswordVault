311982673275ac1dfe87e34725e08964da4ad8b5
kwayne <wkudoi@gmail.com>
2019-10-09 11:26:09 +0300

    methods to add remove credentials

--- credential.py
+++ credential.py
@@ -4,7 +4,7 @@ import pyperclip
 import secrets
 
 
-class User:
+class Credentials:
     """
     #this class creates an instance of a user-credentials
 
@@ -12,3 +12,58 @@ class User:
     credentials_list = []  # set an empty array of expected credentials.
 
     def __init__(self, app_name, account_name, account_password):
+
+        """
+        #user credentials parsing
+
+        """
+        self.app_name = app_name
+        self.account_name = account_name
+        self.account_password = account_password
+
+    def credential_create(self):
+        #create credentials  the populate credentials_list
+        Credentials.credentials_list.append(self)
+
+    def credential_remove(self):
+        #remove credential from credentials_list
+
+    @classmethod
+    def search_name(cls, name):
+            '''
+            #search method to parse in name and return credentials matching 
+                the name.
+        
+            '''
+            for credentials in cls.credentials_list:
+                if credentials.app_name == name:
+                    return credentials
+
+    @classmethod
+    def search_duplicate(cls, appname):
+        '''
+        #confirm to check availability of name to prevent duplicate
+
+        '''
+        for Credentials in cls.credentials_list:
+            if Credentials.app_name == appname:
+                return True
+        return False
+
+    @classmethod
+    def credentials_show(cls):
+        '''
+        #display list of creditials
+
+        '''
+        return cls.credential_list
+
+    @classmethod
+    def copycat(cls, appname):
+        """
+        #method to allow copying of the credentials by user
+
+        """
+        #pyperclip module allows copy pasting 
+        credential_found = Credentials.find_credentialbyappname(appname)
+        pyperclip.copy(credential_found.account_password)
