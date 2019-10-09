5b540baa0df1a2b930a944c27490f492e1775c04
kwayne <wkudoi@gmail.com>
2019-10-09 12:47:12 +0300

    add test method all the users

--- user_test.py
+++ user_test.py
@@ -8,7 +8,7 @@ class TestUser(unittest.TestCase):
         method to run before each test
 
         """
-        # instantiate an object
+        # instantiate an object by populating with dummy values.
         self.new_user = User("Wayne", "Ghost", "OGhost", "0724443879","oghost@gmail.com", "daimaMkenya001") 
     
     def tearDown(self):
@@ -23,13 +23,27 @@ class TestUser(unittest.TestCase):
         subject test for intialization
 
         """
-        self.assertEqual(self.new_user.first_name, "mary")
-        self.assertEqual(self.new_user.last_name, "mbugua")
-        self.assertEqual(self.new_user.user_name, "nish")
-        self.assertEqual(self.new_user.phone_number, "0789755096")
-        self.assertEqual(self.new_user.email, "mary@gmail.com")
-        self.assertEqual(self.new_user.password, "lovepy")
-    
+        self.assertEqual(self.new_user.first_name, "Wayne")
+        self.assertEqual(self.new_user.last_name, "Ghost")
+        self.assertEqual(self.new_user.user_name, "OGhost")
+        self.assertEqual(self.new_user.phone_number, "0724443879")
+        self.assertEqual(self.new_user.email, "oghost@gmail.com")
+        self.assertEqual(self.new_user.password, "daimaMkenya001")
+     
+    def test_user_create(self):
+        """ 
+        #determine is the user list is populated and details saved
+        
+        """
+        self.new_user.user_create()
+        self.assertEqual(len(User.user_list),1)
+
+    def test_user_show(self):
+        """
+        #test method to display users
+        
+        """
+        self.assertEqual(User.user_show(), User.user_list)
      
 if __name__ == '__main__':
     unittest.main()
